90e3325608f81998cceb7233806aa20ddc78f397
Your Name <you@example.com>
2021-04-01 18:43:02 +0300

    30-03 middle of hw

--- 30-03-hw/classes.php
+++ 30-03-hw/classes.php
@@ -1,4 +1,46 @@
 <?php
-abstract class Book{
+
+abstract class Book
+{
+   public $id;
+   public $name;
+   public $author;
+   public $filePath;
+   public $sortOrder;
+   public function __construct(int $id, string $name, string $author, string $filePath, int $sortOrder)
+   {
+       $this->id = $id;
+       $this->name = $name;
+       $this->author = $author;
+       $this->filePath = $filePath;
+       $this->sortOrder = $sortOrder;
+   }
+   abstract public function printInfo();
+
+}
+class BookPdf extends Book
+{
+    final public function printInfo()
+    {
+        return $this->author . $this->name;
+
+    }
+}
+class BookTxt extends Book
+{
+    final public function printInfo()
+    {
+        return $this->author . $this->name;
+
+    }
+
+}
+class BookDoc extends Book
+{
+    final public function printInfo()
+    {
+        return $this->author . $this->name;
+
+    }
 
 }
